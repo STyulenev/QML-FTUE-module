53f0c9ce200151eb55790ee0602b8aa4b3abfd4d
Sergey Tyulenev <tyulenev.i@yandex.ru>
2024-07-05 16:36:52 +0300

    Added module

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
-int main(int argc, char *argv[])
+auto main(int argc, char* argv[]) -> int
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/untitled3/Main.qml"));
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
+    engine.addImportPath("qrc:/qml");
+
+    const QUrl url("qrc:/main.qml");
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app, [url](QObject* obj, const QUrl& objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
     engine.load(url);
 
     return app.exec();
